f7d9ff6dd4f651191d56c94ff66702bb2df81abc
hblok <hblok>
2026-01-31 21:23:49 +0100

    dummy test works

---
+++ maxbloks/fish/tests/test_fish_game.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2025 H. Blok
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
+Unit Tests for fish.py
+"""
+
+import unittest
+
+from maxbloks.fish import fish_game
+
+
+class TestFishGame(unittest.TestCase):
+
+    def test_dummy(self):
+        pass
+    
