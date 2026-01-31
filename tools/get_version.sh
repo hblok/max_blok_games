af45ad4b1204b23453ccfe9756e092328e5eb213
hblok <hblok>
2026-01-31 21:06:32 +0100

    workflows and release

---
+++ tools/get_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Extract version from git tag
+TAG=$(git describe --tags --always 2>/dev/null)
+
+if [ -z "$VERSION" ]; then
+    VERSION_NAME="0.0.0-dev"
+else
+    VERSION_NAME="${VERSION#v}"
+fi
+
+export RELEASE_VERSION=$(echo $VERSION_NAME | cut -f 1 -d '-')
+export RELEASE_NAME=$(echo $VERSION_NAME | cut -f 2 -d '-')
+
